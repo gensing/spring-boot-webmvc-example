a002045259189c72b2aa13f3ebc5eece20fcc3ee
gensing <tohvv15h4@naver.com>
2023-02-13 08:04:21 +0900

    구동시 프로퍼티 로그 확인 추가

--- src/main/java/com/tensing/boot/BootApplication.java
+++ src/main/java/com/tensing/boot/BootApplication.java
@@ -1,19 +1,35 @@
 package com.tensing.boot;
 
+import com.tensing.boot.configuration.properties.TensingProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
-public class BootApplication {
+public class BootApplication implements ApplicationRunner {
+
+    private final TensingProperties tensingProperties;
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(BootApplication.class, args);
         DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
     }
 
-    // 오른쪽 순으로 가장 최신 방식
-    // CommandLineRunner -> ApplicationRunner -> @EventListener : 최초 구동시 실행 되여야 하는 로직이 필요하실 해당 방법 사용
+    // CommandLineRunner -> ApplicationRunner -> @EventListener (오른쪽 순으로 가장 최신 방식)
+    // 최초 구동시 실행 되여야 하는 로직이 필요하실 해당 방법 사용
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.debug("======================");
+        log.debug(tensingProperties.toString());
+        log.debug("======================");
+    }
+
 }
