a4e0eeb1fe2b85557b9e9cf52ed62a85af3316c5
gensing <tohvv15h4@naver.com>
2023-02-13 08:02:57 +0900

    springdoc 추가

---
+++ src/main/java/com/tensing/boot/configuration/OpenApiConfiguration.java
@@ -0,0 +1,22 @@
+package com.tensing.boot.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "spring commerce API 명세서",
+                description = "API 명세서",
+                version = "v1",
+                contact = @Contact(
+                        name = "beginners",
+                        email = "sample@email.co.kr"
+                )
+        )
+)
+@Configuration
+public class OpenApiConfiguration {
+
+}
