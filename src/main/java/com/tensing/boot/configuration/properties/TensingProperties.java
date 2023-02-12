087a3ec66c760a1f137c09e54a47102165c5860a
gensing <tohvv15h4@naver.com>
2023-02-13 08:04:05 +0900

    프로퍼티 클래스 수정

--- src/main/java/com/tensing/boot/configuration/properties/TensingProperties.java
+++ src/main/java/com/tensing/boot/configuration/properties/TensingProperties.java
@@ -1,20 +1,24 @@
 package com.tensing.boot.configuration.properties;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 
-// boot 3버전 부터는 ConstructorBinding 이 default 적용되는 것 같다.
-
-
-@Getter
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "tensing")
-public final class TensingProperties {
-    private final boolean enable;
-    
-    // 클래스를 넣을 때도 final 로
-    // private final Api api;
+public record TensingProperties(
+        SwaggerProperties swagger,
+        JwtProperties jwt
+) {
+    public record JwtProperties(
+            String tokenName,
+            String secretKey,
+            int expirationInMs,
+            SignatureAlgorithm signatureAlgorithm
+    ) {
+    }
 
+    public record SwaggerProperties(
+            boolean enable
+    ) {
+    }
 }
