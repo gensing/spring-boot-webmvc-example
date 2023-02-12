06b4727f8336194d114eeded9842779985630974
gensing <tohvv15h4@naver.com>
2023-02-13 08:03:25 +0900

    싱크 조정

--- src/main/java/com/tensing/boot/configuration/FilterConfiguration.java
+++ src/main/java/com/tensing/boot/configuration/FilterConfiguration.java
@@ -1,33 +1,21 @@
 package com.tensing.boot.configuration;
 
-import com.tensing.boot.global.filter.MDCLoggingFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import com.tensing.boot.global.filters.MDCLoggingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class FilterConfiguration {
 
-    // 로그 인코딩 설정
-    @Bean
-    public FilterRegistrationBean<CharacterEncodingFilter> encodingFilterBean() {
-        FilterRegistrationBean<CharacterEncodingFilter> registrationBean = new FilterRegistrationBean<CharacterEncodingFilter>();
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setForceEncoding(true); // request, response encoding을 같게 설정한다.
-        characterEncodingFilter.setEncoding("UTF-8");
-        registrationBean.setFilter(characterEncodingFilter);
-        return registrationBean;
-    }
-
     // 로그 트랜잭션 번호 생성 필터 등록
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public MDCLoggingFilter mdcLoggingFilter() {
-        return new MDCLoggingFilter();
+        MDCLoggingFilter mdcLoggingFilter = new MDCLoggingFilter();
+        return mdcLoggingFilter;
     }
 
     // 공용 요청 로그 필터 등록
--- src/main/java/com/tensing/boot/configuration/WebMvcConfig.java
+++ src/main/java/com/tensing/boot/configuration/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.tensing.boot.configuration;
 
-import com.tensing.boot.test.TestInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TestInterceptor())
-                .addPathPatterns("/test", "/test/**")
-                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+
     }
 }
