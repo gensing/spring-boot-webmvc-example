4f9da1207bc38b5a695d4e37494fedc83e1199f2
gensing <tohvv15h4@naver.com>
2023-03-03 03:16:23 +0900

    미사용 줄 제거

--- src/main/java/com/tensing/boot/BootApplication.java
+++ src/main/java/com/tensing/boot/BootApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.DispatcherServlet;
 
 
