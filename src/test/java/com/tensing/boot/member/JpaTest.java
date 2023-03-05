68c408dc50f97e910f7dce8c4b30026ea130e392
gensing <tohvv15h4@naver.com>
2023-03-05 09:18:00 +0900

    member repository 단위 테스트 추가

---
+++ src/test/java/com/tensing/boot/member/JpaTest.java
@@ -0,0 +1,36 @@
+package com.tensing.boot.member;
+
+import com.tensing.boot.api.member.entity.Member;
+import com.tensing.boot.api.member.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+
+
+
+@ActiveProfiles("test")
+@SpringBootTest // @DataJpaTest 사용시 불러오지 못하는 빈으로 인하여, 잠시 @SpringBootTest 사용
+public class JpaTest {
+
+    @Autowired
+    private MemberRepository memberRepositoryImpl;
+
+    @Test
+    void save() {
+
+        // given
+        final Member member = Member.builder().username("boot").password("boot1234").email("boot@boot.com").build();
+
+        // when
+        final Member saveMember = memberRepositoryImpl.save(member);
+
+        // then
+        assertEquals("username 이 다릅니다.", member.getUsername(), saveMember.getUsername());
+        assertEquals("email 이 다릅니다.", member.getEmail(), member.getEmail());
+        assertEquals("비밀번호가 다릅니다.", member.getPassword(), member.getPassword());
+    }
+}
