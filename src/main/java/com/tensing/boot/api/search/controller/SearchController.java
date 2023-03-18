65a5460217ab890e3f37702ce850436e0409feee
gensing <tohvv15h4@naver.com>
2023-03-18 13:45:20 +0900

    api endpoint 수정

--- src/main/java/com/tensing/boot/api/search/controller/SearchController.java
+++ src/main/java/com/tensing/boot/api/search/controller/SearchController.java
@@ -5,22 +5,22 @@ import com.tensing.boot.api.search.dto.SearchCondition;
 import com.tensing.boot.api.search.service.EsSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/search")
+@RequestMapping("/api/searches")
 @RestController
 public class SearchController {
 
     private final EsSearchService esSearchService;
 
-    @GetMapping("/search")
-    public List<SearchDocument> search(SearchCondition searchCondition, Pageable pageable) {
+    @GetMapping("")
+    public List<SearchDocument> search(SearchCondition searchCondition, @PageableDefault(page = 1, size = 10) Pageable pageable) {
         return esSearchService.search(searchCondition, pageable);
     }
 }
