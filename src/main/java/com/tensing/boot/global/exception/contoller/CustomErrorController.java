54852edac566751a9d16842399c284bb07b57ace
gensing <tohvv15h4@naver.com>
2023-03-20 19:51:22 +0900

    error 컨트롤러 스웨거 제거

--- src/main/java/com/tensing/boot/global/exception/contoller/CustomErrorController.java
+++ src/main/java/com/tensing/boot/global/exception/contoller/CustomErrorController.java
@@ -1,6 +1,7 @@
 package com.tensing.boot.global.exception.contoller;
 
 import com.tensing.boot.global.exception.payload.ErrorMessages;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 public class CustomErrorController implements ErrorController {
 
