5fe4e4c13c02af4830f341805df73279122d4a8e
gensing <tohvv15h4@naver.com>
2023-03-27 05:05:44 +0900

    implements MemberEntityDao 추가 ( 오류 수정 )

--- src/main/java/com/tensing/boot/application/member/dao/impl/MemberEntityDaoImpl.java
+++ src/main/java/com/tensing/boot/application/member/dao/impl/MemberEntityDaoImpl.java
@@ -2,9 +2,10 @@ package com.tensing.boot.application.member.dao.impl;
 
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tensing.boot.application.member.dao.MemberEntityDao;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class MemberEntityDaoImpl {
+public class MemberEntityDaoImpl implements MemberEntityDao {
     private final JPAQueryFactory jpaQueryFactory;
 }
