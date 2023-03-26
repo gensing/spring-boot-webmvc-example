1477224473ed3d0cfa8b915709ea65111d884879
gensing <tohvv15h4@naver.com>
2023-03-27 08:35:56 +0900

    elasticsearch 테스트용 컨테이너 설정 ( 매우 느리다... 임베디드 방식을 옛날에 지원했다고 하는데.. 해당 방법이 지금도 가능한지 확인 필요. )

---
+++ src/test/java/com/tensing/boot/config/ElasticTestContainer.java
@@ -0,0 +1,46 @@
+package com.tensing.boot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+
+@Slf4j
+@Profile("test")
+@Configuration
+public class ElasticTestContainer extends ElasticsearchConfiguration {
+
+    @Container
+    private static GenericContainer elasticsearchContainer;
+
+    static {
+        elasticsearchContainer = new GenericContainer(
+                new ImageFromDockerfile()
+                        .withDockerfileFromBuilder(builder -> {
+                            builder
+                                    // ES 이미지 가져오기
+                                    .from("docker.elastic.co/elasticsearch/elasticsearch:7.15.2")
+                                    // nori 분석기 설치
+                                    .run("bin/elasticsearch-plugin install analysis-nori")
+                                    .build();
+                        })
+        )
+                .withExposedPorts(9200, 9300)
+                .withClasspathResourceMapping("/docker/elasticsearch/config/dic", "/usr/share/elasticsearch/config/dic", BindMode.READ_ONLY)
+                .withEnv("discovery.type", "single-node");
+        elasticsearchContainer.start();
+    }
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(String.format("%s:%d", elasticsearchContainer.getHost(), elasticsearchContainer.getMappedPort(9200)))
+                .build();
+    }
+
+}
