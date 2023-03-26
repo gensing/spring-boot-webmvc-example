21fffc062c5f04db5529fb7119ad71ec548ede7c
gensing <tohvv15h4@naver.com>
2023-03-27 05:06:52 +0900

    kurent 핸들러 제거

--- src/main/java/com/tensing/boot/config/WebSocketConfiguration.java
+++ src/main/java/com/tensing/boot/config/WebSocketConfiguration.java
@@ -1,29 +1,24 @@
 package com.tensing.boot.config;
 
 import com.tensing.boot.application.webrtc.SocketHandler;
-import com.tensing.boot.application.webrtc.KurentoCallHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 
 @RequiredArgsConstructor
-@Configuration
-@EnableWebSocket
+//@Configuration
+//@EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
-    private final KurentoCallHandler kurentoCallHandler;
     private final SocketHandler socketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(socketHandler, "/socket")
                 .setAllowedOrigins("*");
-        registry.addHandler(kurentoCallHandler, "/call");
     }
 
     @Bean
