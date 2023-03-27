36610748d16f082da89c00b93ff08459a561ea34
gensing <tohvv15h4@naver.com>
2023-03-28 06:51:21 +0900

    doc 생성 로직 추가

--- src/test/java/com/tensing/boot/application/member/MemberControllerTest.java
+++ src/test/java/com/tensing/boot/application/member/MemberControllerTest.java
@@ -1,46 +1,40 @@
 package com.tensing.boot.application.member;
 
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tensing.boot.application.member.model.dto.MemberDto;
-import com.tensing.boot.application.member.service.MemberService;
 import com.tensing.boot.config.security.WithMockCustomUser;
 import com.tensing.boot.global.filters.security.model.code.RoleCode;
-import com.tensing.boot.global.filters.security.model.dto.SecurityDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.nio.charset.StandardCharsets;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @Slf4j
 @ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @SpringBootTest
 public class MemberControllerTest {
 
@@ -57,53 +51,74 @@ public class MemberControllerTest {
     @DisplayName("유저 생성")
     void postTest() throws Exception {
 
+        // given
         final var body = objectMapper.writeValueAsString(memberRequest);
 
         log.info("******** START : MOC MVC test **********");
-        mockMvc.perform(post("/api/members").contentType(MediaType.APPLICATION_JSON).content(body))
-                .andExpect(status().isCreated())
-                .andExpect(header().string("Location", "/api/members/1"))
-                .andDo(print())
-        // rest-docs 문서 생성
-//                .andDo(document("{class-name}/{method-name}",
-//                        requestFields(
-//                                fieldWithPath(MemberDto.MemberRequest.Fields.username).description("The username of a new member"),
-//                                fieldWithPath(MemberDto.MemberRequest.Fields.email).description("The email of a new member"),
-//                                fieldWithPath(MemberDto.MemberRequest.Fields.password).description("The password of a new member")
-//                        ),
-//                        responseHeaders(
-//                                headerWithName("Location").description("review detail resource id")
-//                        )
-//                ))
-        ;
+
+        // when
+        var perform = mockMvc.perform(post("/api/members")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(body));
+
+        // then
+        perform.andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/members/1"));
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+                resource(ResourceSnippetParameters.builder()
+                        .tag("유저 api")
+                        .description("유저 생성 API")
+                        .requestFields(
+                                fieldWithPath(MemberDto.MemberRequest.Fields.username).description("The username of a new member"),
+                                fieldWithPath(MemberDto.MemberRequest.Fields.email).description("The email of a new member"),
+                                fieldWithPath(MemberDto.MemberRequest.Fields.password).description("The password of a new member")
+                        )
+                        .responseHeaders(
+                                headerWithName("Location").description("review detail resource id")
+                        )
+                        .build())
+        ));
         log.info("******** END : MOC MVC test **********");
     }
 
-    @Test
+    //@Test
     @Order(1)
     @WithMockCustomUser(id = 1, role = RoleCode.USER)
     @DisplayName("유저 정보 확인")
     void getTest() throws Exception {
 
         log.info("******** START : MOC MVC test **********");
-        mockMvc.perform(get("/api/members/{id}", 1).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
+
+        // when
+        var perform = mockMvc.perform(get("/api/members/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8));
+
+        // then
+        perform.andExpect(status().isOk())
                 .andExpect(jsonPath(MemberDto.MemberRequest.Fields.username).value(memberRequest.getUsername()))
-                .andExpect(jsonPath(MemberDto.MemberRequest.Fields.email).value(memberRequest.getEmail()))
-                .andDo(print())
-        // rest-docs 문서 생성
-//                .andDo(document("{class-name}/{method-name}",
-//                        requestHeaders(
-//                                headerWithName(HttpHeaders.AUTHORIZATION).description("jwt token")
-//                        ),
-//                        pathParameters(
-//                                parameterWithName("id").description("The id of the member")),
-//                        responseFields(
-//                                fieldWithPath(MemberDto.MemberRequest.Fields.username).description("The username of the member"),
-//                                fieldWithPath(MemberDto.MemberRequest.Fields.email).description("The email of the member")
-//                        )
-//                ))
-        ;
+                .andExpect(jsonPath(MemberDto.MemberRequest.Fields.email).value(memberRequest.getEmail()));
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+
+                resource(ResourceSnippetParameters.builder()
+                        .tag("유저 api")
+                        .description("유저 조회 API")
+                        .requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("jwt token")
+                        )
+                        .pathParameters(
+                                parameterWithName("id").description("The id of the member"))
+                        .responseFields(
+                                fieldWithPath(MemberDto.MemberRequest.Fields.username).description("The username of the member"),
+                                fieldWithPath(MemberDto.MemberRequest.Fields.email).description("The email of the member")
+                        )
+                        .build())
+        ));
         log.info("******** END : MOC MVC test **********");
     }
 }
