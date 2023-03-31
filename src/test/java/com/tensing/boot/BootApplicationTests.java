126b03b3473e3b7c143937e5c38f1ddbe216a00c
gensing <tohvv15h4@naver.com>
2023-03-31 19:52:15 +0900

    테스트 추가

--- src/test/java/com/tensing/boot/BootApplicationTests.java
+++ src/test/java/com/tensing/boot/BootApplicationTests.java
@@ -1,15 +1,27 @@
 package com.tensing.boot;
 
+import com.tensing.boot.application.member.controller.MemberController;
+import com.tensing.boot.application.post.controller.PostController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class BootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MemberController memberController;
+    @Autowired
+    private PostController postController;
+
+    @Test
+    void contextLoads() {
+        assertThat(memberController).isNotNull();
+        assertThat(postController).isNotNull();
+    }
 
 }
