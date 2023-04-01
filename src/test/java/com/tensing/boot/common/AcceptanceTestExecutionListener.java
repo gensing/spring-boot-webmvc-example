60388805035856174715e47739672f63cc5c562f
gensing <tohvv15h4@naver.com>
2023-04-02 00:21:40 +0900

    통합테스트 db초기화 클래스

---
+++ src/test/java/com/tensing/boot/common/AcceptanceTestExecutionListener.java
@@ -0,0 +1,32 @@
+package com.tensing.boot.common;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+import java.util.List;
+
+public class AcceptanceTestExecutionListener extends AbstractTestExecutionListener {
+
+    @Override
+    public void afterTestMethod(final TestContext testContext) {
+        final JdbcTemplate jdbcTemplate = getJdbcTemplate(testContext);
+        final List<String> truncateQueries = getTruncateQueries(jdbcTemplate);
+        truncateTables(jdbcTemplate, truncateQueries);
+    }
+
+    private JdbcTemplate getJdbcTemplate(final TestContext testContext) {
+        return testContext.getApplicationContext().getBean(JdbcTemplate.class);
+    }
+
+    private List<String> getTruncateQueries(final JdbcTemplate jdbcTemplate) {
+        return jdbcTemplate.queryForList("SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'", String.class);
+    }
+
+    private void truncateTables(final JdbcTemplate jdbcTemplate, final List<String> truncateQueries) {
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
+        truncateQueries.forEach(v -> jdbcTemplate.execute(v));
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
+    }
+
+}
