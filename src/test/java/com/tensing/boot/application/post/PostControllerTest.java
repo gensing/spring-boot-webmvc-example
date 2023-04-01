fc5bc5b541e4517a22c7f6bbc8c8990d5d732544
gensing <tohvv15h4@naver.com>
2023-04-02 00:26:55 +0900

    docs 추가

--- src/test/java/com/tensing/boot/application/post/PostControllerTest.java
+++ src/test/java/com/tensing/boot/application/post/PostControllerTest.java
@@ -1,31 +1,37 @@
 package com.tensing.boot.application.post;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tensing.boot.application.member.dao.MemberEntityRepository;
+import com.tensing.boot.common.AcceptanceTestExecutionListener;
 import com.tensing.boot.application.member.model.dto.MemberDto;
 import com.tensing.boot.application.member.service.MemberService;
-import com.tensing.boot.application.post.dao.PostEntityRepository;
 import com.tensing.boot.application.post.model.dto.PostDto;
 import com.tensing.boot.application.post.service.PostService;
 import com.tensing.boot.global.filters.security.Const;
 import com.tensing.boot.global.filters.security.model.dto.SecurityDto;
 import com.tensing.boot.global.filters.security.service.SecurityService;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.charset.StandardCharsets;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,29 +39,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Slf4j
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @SpringBootTest
+@TestExecutionListeners(value = {AcceptanceTestExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 public class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
-
     @Autowired
     private PostService postService;
-
     @Autowired
     private MemberService memberService;
-
     @Autowired
     private SecurityService securityService;
-
-    @Autowired
-    private MemberEntityRepository memberEntityRepository;
-
-    @Autowired
-    private PostEntityRepository postEntityRepository;
 
     private String bearerToken;
 
@@ -77,7 +75,7 @@ public class PostControllerTest {
                 .password("test1234@T")
                 .build();
 
-        var memberId = memberService.createMember(memberRequest);
+        var memberId = memberService.createMember(memberRequest).getId();
 
         var tokenRequest = SecurityDto.TokenRequest.builder()
                 .grantType(SecurityDto.GranType.ISSUE)
@@ -88,13 +86,6 @@ public class PostControllerTest {
         this.bearerToken = Const.BEARER_PREFIX + securityService.getToken(tokenRequest).getAccessToken();
 
         this.postId = postService.insert(postRequest, SecurityDto.UserInfo.builder().id(memberId).build());
-    }
-
-
-    @AfterEach
-    void tearDown() {
-        postEntityRepository.deleteAll();
-        memberEntityRepository.deleteAll();
     }
 
     @Test
@@ -108,12 +99,29 @@ public class PostControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding(StandardCharsets.UTF_8)
                 .header(HttpHeaders.AUTHORIZATION, bearerToken)
-                .header(HttpHeaders.AUTHORIZATION + "2", bearerToken)
                 .content(objectMapper.writeValueAsString(postRequest))
         );
 
         // then
         perform.andExpect(status().isCreated());
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+                resource(ResourceSnippetParameters.builder()
+                        .tag("post 생성 API")
+                        .description("post 생성 API")
+                        .requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("jwt token")
+                        )
+                        .requestFields(
+                                fieldWithPath(PostDto.PostRequest.Fields.title).description("The title of a new post"),
+                                fieldWithPath(PostDto.PostRequest.Fields.body).description("The body of a new post")
+                        )
+                        .responseHeaders(
+                                headerWithName("Location").description("review detail resource id")
+                        )
+                        .build())
+        ));
     }
 
     @Test
@@ -126,9 +134,23 @@ public class PostControllerTest {
                 .characterEncoding(StandardCharsets.UTF_8)
         );
 
-        perform.andExpect(status().isOk())
-        //.andExpect(jsonPath("$.*", hasSize(2)))
-        ;
+        perform.andExpect(status().isOk());
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+                resource(ResourceSnippetParameters.builder()
+                        .tag("post list 조회 API")
+                        .description("post list 조회 API")
+                        .responseFields(
+                                fieldWithPath("[]." + PostDto.PostResponse.Fields.id).description("The id of the post"),
+                                fieldWithPath("[]." + PostDto.PostResponse.Fields.writer).description("The writer of the post"),
+                                fieldWithPath("[]." + PostDto.PostResponse.Fields.title).description("The title of the post"),
+                                fieldWithPath("[]." + PostDto.PostResponse.Fields.body).description("The body of the post"),
+                                fieldWithPath("[]." + PostDto.PostResponse.Fields.createdDate).description("The createdDate of the post"),
+                                fieldWithPath("[]." + PostDto.PostResponse.Fields.updatedDate).description("The updatedDate of the post")
+                        )
+                        .build())
+        ));
     }
 
     @Test
@@ -136,7 +158,7 @@ public class PostControllerTest {
     void getTest() throws Exception {
 
         // when
-        var perform = mockMvc.perform(get("/api/posts/{postId}", postId)
+        var perform = mockMvc.perform(get("/api/posts/{id}", postId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding(StandardCharsets.UTF_8)
         );
@@ -146,6 +168,24 @@ public class PostControllerTest {
                 .andExpect(jsonPath(PostDto.PostResponse.Fields.id).value(postId))
                 .andExpect(jsonPath(PostDto.PostResponse.Fields.title).value(postRequest.getTitle()))
                 .andExpect(jsonPath(PostDto.PostResponse.Fields.body).value(postRequest.getBody()));
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+                resource(ResourceSnippetParameters.builder()
+                        .tag("post 조회 API")
+                        .description("post 조회 API")
+                        .pathParameters(
+                                parameterWithName("id").description("The id of the post"))
+                        .responseFields(
+                                fieldWithPath(PostDto.PostResponse.Fields.id).description("The id of the post"),
+                                fieldWithPath(PostDto.PostResponse.Fields.writer).description("The writer of the post"),
+                                fieldWithPath(PostDto.PostResponse.Fields.title).description("The title of the post"),
+                                fieldWithPath(PostDto.PostResponse.Fields.body).description("The body of the post"),
+                                fieldWithPath(PostDto.PostResponse.Fields.createdDate).description("The createdDate of the post"),
+                                fieldWithPath(PostDto.PostResponse.Fields.updatedDate).description("The updatedDate of the post")
+                        )
+                        .build())
+        ));
     }
 
     @Test
@@ -153,9 +193,12 @@ public class PostControllerTest {
     void putTest() throws Exception {
 
         // when
-        var postRequest = PostDto.PostPutRequest.builder().title("test update title 1").body("test update body 1").build();
+        var postRequest = PostDto.PostPutRequest.builder()
+                .title("test update title 1")
+                .body("test update body 1")
+                .build();
 
-        var perform = mockMvc.perform(put("/api/posts/{postId}", postId)
+        var perform = mockMvc.perform(put("/api/posts/{id}", postId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding(StandardCharsets.UTF_8)
                 .header(HttpHeaders.AUTHORIZATION, bearerToken)
@@ -164,6 +207,16 @@ public class PostControllerTest {
 
         // then
         perform.andExpect(status().isOk());
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+                resource(ResourceSnippetParameters.builder()
+                        .tag("post 수정 API")
+                        .description("post 수정 API")
+                        .pathParameters(
+                                parameterWithName("id").description("The id of the post"))
+                        .build())
+        ));
     }
 
     @Test
@@ -171,7 +224,7 @@ public class PostControllerTest {
     void deleteTest() throws Exception {
 
         // when
-        var perform = mockMvc.perform(delete("/api/posts/{postId}", postId)
+        var perform = mockMvc.perform(delete("/api/posts/{id}", postId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding(StandardCharsets.UTF_8)
                 .header(HttpHeaders.AUTHORIZATION, bearerToken)
@@ -179,6 +232,15 @@ public class PostControllerTest {
 
         // then
         perform.andExpect(status().isOk());
+
+        // docs
+        perform.andDo(document("{class-name}/{method-name}",
+                resource(ResourceSnippetParameters.builder()
+                        .tag("post 삭제 API")
+                        .description("post 삭제 API")
+                        .pathParameters(
+                                parameterWithName("id").description("The id of the post"))
+                        .build())));
     }
 
 }
