6b28f83d04b9bd0f9eda7d7561a0f7310ec8a813
gensing <tohvv15h4@naver.com>
2023-04-05 12:55:11 +0900

    native query 로 변경 ( dsl 사용 )

--- src/main/java/com/tensing/boot/application/post/dao/impl/PostDocumentDaoImpl.java
+++ src/main/java/com/tensing/boot/application/post/dao/impl/PostDocumentDaoImpl.java
@@ -1,13 +1,13 @@
 package com.tensing.boot.application.post.dao.impl;
 
 import com.tensing.boot.application.post.dao.PostDocumentDao;
-import com.tensing.boot.application.post.model.dto.SearchCondition;
 import com.tensing.boot.application.post.model.data.PostDocument;
+import com.tensing.boot.application.post.model.dto.SearchCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.Criteria;
-import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -15,31 +15,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostDocumentDaoImpl implements PostDocumentDao {
 
-    // CriteriaQuery, StringQuery, NativeSearchQuery
+
     private final ElasticsearchOperations operations;
 
     public List<PostDocument> search(SearchCondition searchCondition, Pageable pageable) {
-        var query = createConditionCriteriaQuery(searchCondition);
-        // page 시작은 0번 부터, sort 필드 체크 기능 필요 ( 실제로 있는 필드만 받을 수 있도록, integer 필드 정렬 안됨 확인 필요. )
-        pageable.getSort();
-        query.setPageable(pageable);
-        var search = operations.search(query, PostDocument.class);
-        return search.stream()
+
+        // CriteriaQuery, StringQuery, NativeSearchQuery
+        Query query = NativeQuery.builder()
+                .withQuery(q -> q
+                        .bool(b -> {
+                                    if (StringUtils.hasText(searchCondition.getSearch())) {
+                                        var search = searchCondition.getSearch();
+                                        b.should(bm -> bm.match(m -> m.field(PostDocument.Fields.title).query(search)));
+                                        b.should(bm -> bm.match(m -> m.field(PostDocument.Fields.description).query(search)));
+                                    }
+                                    return b;
+                                }
+                        )
+                )
+                .withPageable(pageable)
+                .build();
+
+        return operations.search(query, PostDocument.class).stream()
                 .map(i -> i.getContent())
                 .toList();
     }
 
-    private CriteriaQuery createConditionCriteriaQuery(SearchCondition searchCondition) {
-        CriteriaQuery query = new CriteriaQuery(new Criteria());
-
-        if (searchCondition == null)
-            return query;
-
-        if (StringUtils.hasText(searchCondition.getSearch())) {
-            query.addCriteria(Criteria.where("title").is(searchCondition.getSearch()));
-            query.addCriteria(Criteria.where("description").is(searchCondition.getSearch()));
-        }
-
-        return query;
-    }
 }
