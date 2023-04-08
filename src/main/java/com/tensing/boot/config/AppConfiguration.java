2e5dab1457a706f4b438d914ebcdcaea31aa57f1
gensing <tohvv15h4@naver.com>
2023-04-09 06:38:22 +0900

    objectmapper 설정 변경

--- src/main/java/com/tensing/boot/config/AppConfiguration.java
+++ src/main/java/com/tensing/boot/config/AppConfiguration.java
@@ -1,23 +1,15 @@
 package com.tensing.boot.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.tensing.boot.config.properties.JwtProperties;
 import com.tensing.boot.common.modules.TokenProvider;
+import com.tensing.boot.config.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -47,28 +39,9 @@ public class AppConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(javaTimeModule());
-        return objectMapper;
-    }
-
-    @Bean
-    public JavaTimeModule javaTimeModule(){
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-
-        // formatter
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        // deserializers
-        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(dateFormatter));
-        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
-
-        // serializers
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(dateFormatter));
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
-
-        return javaTimeModule;
+        return new ObjectMapper()
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // 객체가 아닌 string 으로 표현
+                .registerModules(new JavaTimeModule());
     }
 
 }
